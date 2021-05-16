1b84fc79f829d2eeaf01e4d4f1a66da4861eaf58
NataliaRN <natita1714@gmail.com>
2021-05-16 17:54:08 -0400

    Modificacion HTM, CCS, Javascript

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,20 @@
-const validator = {
-  // ...
-};
 
-export default validator;
+function validate(n){
+    const digits = n.toString().split('').map(Number);
+
+    // if it´s an even number
+    if(digits.length % 2 ===0){
+        
+    }
+
+    console.log(digits)
+
+}
+var owner = $('#owner'),
+    cardNumber = $('#cardNumber'),
+    cardNumberField = $('#card-number-field'),
+    CVV = $("#cvv"),
+    mastercard = $("#mastercard"),
+    confirmButton = $('#confirm-purchase'),
+    visa = $("#visa"),
+    
